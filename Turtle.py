31fd7bb32c72d1befb341cbbf20d3656e3c9e9e0
BRIANSTEEDLEY <bsteedley99@gmail.com>
2017-01-24 11:19:00 -0800

    smiley

--- Turtle.py
+++ Turtle.py
@@ -1,1 +1,44 @@
 """ Create a Turtle, name it, make it BLUE and draw a Smiley Face"""
+import turtle
+
+
+smiles = turtle.Turtle()
+
+smiles.speed(5)
+
+smiles.begin_fill()
+smiles.color("BLUE")
+smiles.penup()
+smiles.goto(0,-35)
+smiles.pendown()
+smiles.circle(140, 360)
+smiles.end_fill()
+
+
+smiles.begin_fill()
+smiles.color('Black')
+smiles.penup()
+smiles.goto(-75,150)
+smiles.pendown()
+smiles.circle(10)
+smiles.end_fill()
+
+smiles.begin_fill()
+smiles.penup()
+smiles.goto(75,150)
+smiles.pendown()
+smiles.circle(10)
+smiles.end_fill()
+
+
+smiles.penup()
+smiles.goto(0,0)
+smiles.pendown()
+smiles.circle(100,90)
+
+
+smiles.penup()
+smiles.setheading(180)
+smiles.goto(0,0)
+smiles.pendown()
+smiles.circle(-100,90)
