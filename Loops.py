9b78cc0fef32de6c5069d6fb0b4f06befd4a4ee9
BRIANSTEEDLEY <bsteedley99@gmail.com>
2017-01-25 10:16:41 -0800

    loop

--- Loops.py
+++ Loops.py
@@ -1,2 +1,25 @@
 """Use a loop to make a turtle draw a shape that is has at least 100 sides and
-that shows symmetry.  The entire shape must fit inside the screen""" 
+that shows symmetry.  The entire shape must fit inside the screen"""
+
+import Turtle
+def reset():
+    turtle.clear()
+    turtle.pu()
+    turtle.home()
+    turtle.pd()
+
+x = 100
+y = 100
+theta = 100
+def triangle (x,y,theta):
+    for i in range(1):
+        turtle.pu()
+        turtle.goto(-100,100)
+        turtle.pd()
+        turtle.forward(x)
+        turtle.right(y)
+        turtle.forward(theta)
+        turtle.goto(-100,100)
+
+
+triangle(x,y,theta)
