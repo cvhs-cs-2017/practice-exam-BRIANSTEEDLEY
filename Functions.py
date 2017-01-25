8f6b72c819a3cb1b431813cd63720c8511f960a1
BRIANSTEEDLEY <bsteedley99@gmail.com>
2017-01-25 11:01:54 -0800

    functions

--- Functions.py
+++ Functions.py
@@ -1,4 +1,10 @@
 """1. Write a function that will double any integer (n) and return the result"""
+n = 21
+n + int(input())
+def double(n):
+    n = n + 2
+    return (n)
+    print(double(n))
 
 
 
@@ -7,3 +13,7 @@
 """2.  Write a program that will (1) ask the user for an input value, (2) take
 that and double it and  (3) print the result.
 Include necessary print statements and address whitespace issues."""
+n = int(input('Enter a number'))
+def double(n):
+    n = n + 2
+    print (double(n))
